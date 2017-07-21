09269e69158111cb1ae7dc0c489beb38a7ec6cd2
ayangliming <775391014@example.com>
2017-07-21 11:35:20 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './App.css'
+import HomePage from './HomePage'
+import SignUp from './SignUp'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch
+} from  "react-router-dom"
+class App extends Component {
+
+  render() {
+    return (
+
+        <Router>
+            <Switch>
+                <Route path="/" exact component={HomePage}></Route>
+                <Route path="/signup" component={SignUp}></Route>
+            </Switch>
+
+        </Router>
+
+
+
+    )
+  }
+}
+
+export default App;
